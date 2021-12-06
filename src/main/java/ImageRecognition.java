9219587899450ce38d9f6bf7886e786da88b7074
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2021-12-06 12:13:05 +0000

    Classes

--- src/main/java/ImageRecognition.java
+++ src/main/java/ImageRecognition.java
@@ -4,18 +4,11 @@ import org.openimaj.image.colour.ColourSpace;
 import org.openimaj.image.colour.RGBColour;
 import org.openimaj.image.typography.hershey.HersheyFont;
 
-public class Budapest {
+public class ImageRecognition {
     public static void main(String[] args) {
-        // Create an image
         MBFImage image = new MBFImage(400,70, ColourSpace.RGB);
-
-        // Fill the image with white
         image.fill(RGBColour.WHITE);
-
-        // Render some test into the image - exercise 1
         image.drawText("Hello Budapest!", 10, 60, HersheyFont.CURSIVE, 50, RGBColour.BLACK);
-
-        // Display the image
         DisplayUtilities.display(image);
     }
 }
