107d147ca2a65d4132c6f60f84e91f9a1c54f969
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2021-12-06 15:52:24 +0000

    Structure

--- src/main/java/Classifier2.java
+++ src/main/java/Classifier2.java
@@ -1,15 +1,27 @@
 import de.bwaldvogel.liblinear.SolverType;
+import org.openimaj.data.DataSource;
+import org.openimaj.data.dataset.GroupedDataset;
+import org.openimaj.data.dataset.ListDataset;
 import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.data.dataset.VFSListDataset;
+import org.openimaj.experiment.dataset.sampling.GroupedUniformRandomisedSampler;
 import org.openimaj.feature.DoubleFV;
 import org.openimaj.feature.FeatureExtractor;
 import org.openimaj.feature.SparseIntFV;
+import org.openimaj.feature.local.data.LocalFeatureListDataSource;
+import org.openimaj.feature.local.list.LocalFeatureList;
 import org.openimaj.image.FImage;
 import org.openimaj.image.feature.local.aggregate.BagOfVisualWords;
 import org.openimaj.image.feature.local.aggregate.BlockSpatialAggregator;
+import org.openimaj.image.feature.local.keypoints.Keypoint;
 import org.openimaj.ml.annotation.linear.LiblinearAnnotator;
+import org.openimaj.ml.clustering.ByteCentroidsResult;
 import org.openimaj.ml.clustering.assignment.HardAssigner;
+import org.openimaj.ml.clustering.kmeans.ByteKMeans;
 import org.openimaj.util.pair.IntFloatPair;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Classifier2 {
     private VFSGroupDataset<FImage> training;
@@ -23,21 +35,21 @@ public class Classifier2 {
     /*
     1. Take fixed size densely-sampled pixel patches (8x8 in size and 4 pixels apart in the x and y directions)
     2. Mean centering and normalise the patches
-    3. Extract bovw features from the image patches
+    3. Extract visual words features from the image patches
     4. Use K-Means clustering to group the bags into 1 of 500 clusters
-    5. Create a histogram from the clusters
-    6. Classify the image from the histogram
+    5. Create a BOVW from the clusters
+    6. Classify the image from the BOVW
      */
 
     public void run() {
-        HardAssigner<byte[], float[], IntFloatPair> assigner2 = trainAssigner();
+        HardAssigner<byte[], float[], IntFloatPair> assigner2 = trainAssigner(GroupedUniformRandomisedSampler.sample(training, 20));
         Extractor2 extractor2 = new Extractor2(assigner2);
 
         LiblinearAnnotator liblinearAnnotator = new LiblinearAnnotator<>(extractor2, LiblinearAnnotator.Mode.MULTILABEL, SolverType.L1R_L2LOSS_SVC, 15, 0.1);
         liblinearAnnotator.train(training);
     }
 
-    private static class Extractor2 implements FeatureExtractor<DoubleFV, FImage> {
+    static class Extractor2 implements FeatureExtractor<DoubleFV, FImage> {
         private final HardAssigner<byte[], float[], IntFloatPair> assigner2;
 
         public Extractor2(HardAssigner<byte[], float[], IntFloatPair> assigner2) {
@@ -48,13 +60,27 @@ public class Classifier2 {
         public DoubleFV extractFeature(FImage image) {
             BagOfVisualWords<byte[]> bovw = new BagOfVisualWords<>(assigner2);
 
-            BlockSpatialAggregator<byte[], SparseIntFV> spatial = new BlockSpatialAggregator<>(bovw, 8, 8);
+            BlockSpatialAggregator<byte[], SparseIntFV> spatial = new BlockSpatialAggregator<>(bovw, 1, 1);
 
-            return spatial.aggregate(null /*List of feature vectors*/, image.getBounds()).normaliseFV();
+            return spatial.aggregate(imageFeatures(image), image.getBounds()).normaliseFV();
         }
     }
 
-    HardAssigner<byte[], float[], IntFloatPair> trainAssigner() {
+    HardAssigner<byte[], float[], IntFloatPair> trainAssigner(GroupedDataset<String, ListDataset<FImage>, FImage> sample) {
+        List<LocalFeatureList<Keypoint>> allkeys = new ArrayList<>();
+
+        for (FImage image : sample) {
+            allkeys.add(imageFeatures(image));
+        }
+
+        ByteKMeans km = ByteKMeans.createKDTreeEnsemble(500);
+        DataSource<byte[]> datasource = new LocalFeatureListDataSource<>(allkeys);
+        ByteCentroidsResult result = km.cluster(datasource);
+
+        return result.defaultHardAssigner();
+    }
+
+    static LocalFeatureList<Keypoint> imageFeatures(FImage image) {
         return null;
     }
 }
