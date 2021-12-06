24d15e5c2f6256a936bb8a7878263ec969a28503
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2021-12-06 15:08:33 +0000

    Comments

--- src/main/java/Classifier2.java
+++ src/main/java/Classifier2.java
@@ -1,15 +1,15 @@
 import de.bwaldvogel.liblinear.SolverType;
 import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.data.dataset.VFSListDataset;
+import org.openimaj.feature.DoubleFV;
 import org.openimaj.feature.FeatureExtractor;
-import org.openimaj.feature.FeatureVector;
+import org.openimaj.feature.SparseIntFV;
 import org.openimaj.image.FImage;
 import org.openimaj.image.feature.local.aggregate.BagOfVisualWords;
+import org.openimaj.image.feature.local.aggregate.BlockSpatialAggregator;
 import org.openimaj.ml.annotation.linear.LiblinearAnnotator;
 import org.openimaj.ml.clustering.assignment.HardAssigner;
 import org.openimaj.util.pair.IntFloatPair;
-
-import java.lang.annotation.Annotation;
 
 public class Classifier2 {
     private VFSGroupDataset<FImage> training;
@@ -20,19 +20,24 @@ public class Classifier2 {
         this.testing = testing;
     }
 
+    /*
+    1. Take fixed size densely-sampled pixel patches (8x8 in size and 4 pixels apart in the x and y directions)
+    2. Mean centering and normalise the patches
+    3. Extract bovw features from the image patches
+    4. Use K-Means clustering to group the bags into 1 of 500 clusters
+    5. Create a histogram from the clusters
+    6. Classify the image from the histogram
+     */
+
     public void run() {
         HardAssigner<byte[], float[], IntFloatPair> assigner2 = trainAssigner();
         Extractor2 extractor2 = new Extractor2(assigner2);
 
         LiblinearAnnotator liblinearAnnotator = new LiblinearAnnotator<>(extractor2, LiblinearAnnotator.Mode.MULTILABEL, SolverType.L1R_L2LOSS_SVC, 15, 0.1);
         liblinearAnnotator.train(training);
-
-        for (FImage image : testing) {
-            System.out.println(liblinearAnnotator.annotate(image));
-        }
     }
 
-    private static class Extractor2 implements FeatureExtractor<FeatureVector, FImage> {
+    private static class Extractor2 implements FeatureExtractor<DoubleFV, FImage> {
         private final HardAssigner<byte[], float[], IntFloatPair> assigner2;
 
         public Extractor2(HardAssigner<byte[], float[], IntFloatPair> assigner2) {
@@ -40,10 +45,12 @@ public class Classifier2 {
         }
 
         @Override
-        public FeatureVector extractFeature(FImage image) {
+        public DoubleFV extractFeature(FImage image) {
             BagOfVisualWords<byte[]> bovw = new BagOfVisualWords<>(assigner2);
 
-            return null;
+            BlockSpatialAggregator<byte[], SparseIntFV> spatial = new BlockSpatialAggregator<>(bovw, 8, 8);
+
+            return spatial.aggregate(null /*List of feature vectors*/, image.getBounds()).normaliseFV();
         }
     }
 
