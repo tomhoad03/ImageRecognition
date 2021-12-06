412844e10baf255ff66f1c2fab68309a8995b0fc
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2021-12-06 14:17:14 +0000

    Classifier2 start

--- src/main/java/Classifier2.java
+++ src/main/java/Classifier2.java
@@ -1,12 +1,53 @@
+import de.bwaldvogel.liblinear.SolverType;
+import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.data.dataset.VFSListDataset;
+import org.openimaj.feature.FeatureExtractor;
+import org.openimaj.feature.FeatureVector;
 import org.openimaj.image.FImage;
+import org.openimaj.image.feature.local.aggregate.BagOfVisualWords;
+import org.openimaj.ml.annotation.linear.LiblinearAnnotator;
+import org.openimaj.ml.clustering.assignment.HardAssigner;
+import org.openimaj.util.pair.IntFloatPair;
+
+import java.lang.annotation.Annotation;
 
 public class Classifier2 {
+    private VFSGroupDataset<FImage> training;
     private VFSListDataset<FImage> testing;
-    private VFSListDataset<FImage> training;
 
-    public Classifier2(VFSListDataset<FImage> testing, VFSListDataset<FImage> training) {
-        this.testing = testing;
+    public Classifier2(VFSGroupDataset<FImage> training, VFSListDataset<FImage> testing) {
         this.training = training;
+        this.testing = testing;
+    }
+
+    public void run() {
+        HardAssigner<byte[], float[], IntFloatPair> assigner2 = trainAssigner();
+        Extractor2 extractor2 = new Extractor2(assigner2);
+
+        LiblinearAnnotator liblinearAnnotator = new LiblinearAnnotator<>(extractor2, LiblinearAnnotator.Mode.MULTILABEL, SolverType.L1R_L2LOSS_SVC, 15, 0.1);
+        liblinearAnnotator.train(training);
+
+        for (FImage image : testing) {
+            System.out.println(liblinearAnnotator.annotate(image));
+        }
+    }
+
+    private static class Extractor2 implements FeatureExtractor<FeatureVector, FImage> {
+        private final HardAssigner<byte[], float[], IntFloatPair> assigner2;
+
+        public Extractor2(HardAssigner<byte[], float[], IntFloatPair> assigner2) {
+            this.assigner2 = assigner2;
+        }
+
+        @Override
+        public FeatureVector extractFeature(FImage image) {
+            BagOfVisualWords<byte[]> bovw = new BagOfVisualWords<>(assigner2);
+
+            return null;
+        }
+    }
+
+    HardAssigner<byte[], float[], IntFloatPair> trainAssigner() {
+        return null;
     }
 }
--- src/main/java/ImageRecognition.java
+++ src/main/java/ImageRecognition.java
@@ -1,5 +1,5 @@
+import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.data.dataset.VFSListDataset;
-import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
 
@@ -8,14 +8,14 @@ import java.nio.file.Paths;
 public class ImageRecognition {
     public static void main(String[] args) {
         try {
+            VFSGroupDataset<FImage> training = new VFSGroupDataset<>("zip:" + Paths.get("").toAbsolutePath() + "\\images\\training.zip", ImageUtilities.FIMAGE_READER);
             VFSListDataset<FImage> testing = new VFSListDataset<>("zip:" + Paths.get("").toAbsolutePath() + "\\images\\testing.zip", ImageUtilities.FIMAGE_READER);
-            VFSListDataset<FImage> training = new VFSListDataset<>("zip:" + Paths.get("").toAbsolutePath() + "\\images\\training.zip", ImageUtilities.FIMAGE_READER);
-
-            // DisplayUtilities.display("Testing", testing);
-            // DisplayUtilities.display("Training", training);
 
             Classifier1 classifier1 = new Classifier1(testing, training);
-            Classifier2 classifier2 = new Classifier2(testing, training);
+
+            Classifier2 classifier2 = new Classifier2(training, testing);
+            classifier2.run();
+
             Classifier3 classifier3 = new Classifier3(testing, training);
         } catch (Exception e) {
             e.printStackTrace();
