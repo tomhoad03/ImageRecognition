3ec357abf099d6e27dc64dda62d7875a44e9f427
Tom Hoad <40165392+tomhoad03@users.noreply.github.com>
2021-12-15 17:30:48 +0000

    Absolute beauty

--- src/main/java/Classifier1.java
+++ src/main/java/Classifier1.java
@@ -1,16 +1,101 @@
+import org.openimaj.data.dataset.GroupedDataset;
+import org.openimaj.data.dataset.ListDataset;
 import org.openimaj.data.dataset.VFSGroupDataset;
+import org.openimaj.experiment.evaluation.classification.ClassificationEvaluator;
+import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMAnalyser;
+import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMResult;
 import org.openimaj.image.FImage;
+import org.openimaj.image.processing.resize.ResizeProcessor;
+import org.openimaj.knn.FloatNearestNeighboursExact;
+import org.openimaj.ml.annotation.Annotated;
+import org.openimaj.ml.annotation.BatchAnnotator;
+import org.openimaj.ml.annotation.ScoredAnnotation;
+import org.openimaj.util.array.ArrayUtils;
+import org.openimaj.util.pair.IntFloatPair;
 
-public class Classifier1 {
-    private VFSGroupDataset<FImage> training;
-    private VFSGroupDataset<FImage> testing;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Classifier1 extends Classifier {
+    private final Annotator1 annotator1;
 
     public Classifier1(VFSGroupDataset<FImage> training, VFSGroupDataset<FImage> testing) {
-        this.training = training;
-        this.testing = testing;
+        super(training, testing);
+        this.annotator1 = new Annotator1();
     }
 
-    public void run() throws Exception {
+    public void run() {
+        annotator1.train(getTraining());
+        ClassificationEvaluator<CMResult<String>, String, FImage> evaluator = new ClassificationEvaluator<>(annotator1, getTesting(), new CMAnalyser<>(CMAnalyser.Strategy.SINGLE));
+        setEvaluation(evaluator.evaluate());
+    }
+}
+
+class Annotator1 extends BatchAnnotator<FImage, String> {
+    private Set<String> annotations;
+    private FloatNearestNeighboursExact knn;
+    private String[] trainingAnnotations;
+
+    public FImage makeTiny(FImage image) {
+        int size = Math.min(image.width, image.height);
+
+        image = ResizeProcessor.resample(image.extractCenter(size, size), 16, 16);
+
+        return image.normalise();
+    }
+
+    public float[] flattenImage(FImage image) {
+        return ArrayUtils.concatenate(image.pixels);
+    }
+
+    @Override
+    public Set<String> getAnnotations() {
+        return this.annotations;
+    }
+
+    @Override
+    public List<ScoredAnnotation<String>> annotate(FImage image) {
+        List<IntFloatPair> nearest = this.knn.searchKNN(flattenImage(makeTiny(image)), 10);
+        List<ScoredAnnotation<String>> result = new ArrayList<ScoredAnnotation<String>>(1);
+        nearest.stream()
+                .map(e -> trainingAnnotations[e.getFirst()])
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .ifPresent(e -> result.add(new ScoredAnnotation<String>(e.getKey(), (float) e.getValue() / 15)));
+        return result;
+    }
+
+    @Override
+    public void train(GroupedDataset<String, ? extends ListDataset<FImage>, FImage> training) {
+
+        this.annotations = training.getGroups();
+        float[][] trainingData = new float[training.numInstances()][16*16];
+        this.trainingAnnotations = new String[trainingData.length];
+
+        int imageIndex = 0;
+        for (String instance : training.getGroups()) {
+            System.out.println(instance);
+            for (FImage image : training.get(instance)) {
+                trainingData[imageIndex] = flattenImage(makeTiny(image));
+                trainingAnnotations[imageIndex] = instance;
+                imageIndex++;
+            }
+
+        }
+
+        this.knn = new FloatNearestNeighboursExact(trainingData);
+
+
+    }
+
+    @Override
+    public void train(List<? extends Annotated<FImage, String>> list) {
 
     }
 }
--- src/main/java/ImageRecognition.java
+++ src/main/java/ImageRecognition.java
@@ -1,8 +1,12 @@
 import org.openimaj.data.dataset.VFSGroupDataset;
+import org.openimaj.experiment.evaluation.classification.ClassificationResult;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.nio.file.Paths;
+import java.util.*;
 
 public class ImageRecognition {
     public static void main(String[] args) {
@@ -12,14 +16,41 @@ public class ImageRecognition {
 
             Classifier1 classifier1 = new Classifier1(training, testing);
             classifier1.run();
+            print(classifier1);
 
             Classifier2 classifier2 = new Classifier2(training, testing);
             classifier2.run();
-
-            Classifier3 classifier3 = new Classifier3(training, testing);
-            classifier3.run();
+            print(classifier2);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public static void print(Classifier classifier) throws Exception {
+        System.out.println("Analysing the results of the classification...");
+        File files = new File(Paths.get("").toAbsolutePath() + "\\images\\testing\\testing");
+        ArrayList<String> fileNames = new ArrayList<>(List.of(Objects.requireNonNull(files.list())));
+        ArrayList<String> results = new ArrayList<>(); int count = 0;
+
+        for (FImage image : classifier.getTesting().get("testing")) {
+            for (Map.Entry<FImage, ClassificationResult<String>> evalEntry : classifier.getEvaluation().entrySet()) {
+                if (evalEntry.getKey().equals(image)) {
+                    String fileName = fileNames.get(count); count++;
+                    String prediction = evalEntry.getValue().getPredictedClasses().toString();
+
+                    results.add(fileName + " " + prediction + "\n");
+                    break;
+                }
+            }
+        }
+        results.sort(Comparator.comparing(o -> Integer.parseInt(o.substring(0, o.indexOf(".jpg")))));
+
+        System.out.println("Printing the results of the classification...");
+        FileWriter fileWriter = new FileWriter(Paths.get("").toAbsolutePath() + "\\runs\\run2.txt");
+        for (String result : results) {
+            fileWriter.write(result);
+        }
+        fileWriter.close();
+        System.out.println("Classification has finished...");
     }
 }
