10521b79ee943328faf19a29a185b8a7a85e0a2f
Kynemo <35339148+Kynemo@users.noreply.github.com>
2021-12-15 16:42:13 +0000

    Finished NN implementation

--- src/main/java/Classifier1.java
+++ src/main/java/Classifier1.java
@@ -1,10 +1,13 @@
+import com.android.dx.rop.annotation.AnnotationsList;
 import org.openimaj.data.dataset.GroupedDataset;
 import org.openimaj.data.dataset.ListDataset;
 import org.openimaj.data.dataset.VFSGroupDataset;
+import org.openimaj.data.dataset.VFSListDataset;
 import org.openimaj.experiment.evaluation.classification.ClassificationEvaluator;
 import org.openimaj.experiment.evaluation.classification.ClassificationResult;
 import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMAnalyser;
 import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMResult;
+import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.FImage;
 import org.openimaj.image.processing.resize.ResizeProcessor;
 import org.openimaj.knn.FloatNearestNeighboursExact;
@@ -12,56 +15,17 @@ import org.openimaj.ml.annotation.Annotated;
 import org.openimaj.ml.annotation.BatchAnnotator;
 import org.openimaj.ml.annotation.ScoredAnnotation;
 import org.openimaj.util.array.ArrayUtils;
+import org.openimaj.util.pair.IntFloatPair;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class Classifier1 extends BatchAnnotator<FImage, String> {
-    private VFSGroupDataset<FImage> testing;
-    private VFSGroupDataset<FImage> training;
     private Set<String> annotations;
-
-    public Classifier1(VFSGroupDataset<FImage> testing, VFSGroupDataset<FImage> training) {
-        this.testing = testing;
-        this.training = training;
-        this.annotations = training.getGroups();
-    }
-
-    public void run() {
-
-        float[][] trainingData = new float[training.numInstances()][16*16];
-
-        int imageIndex = 0;
-        for (String instance : training.getGroups()) {
-            for (FImage image : training.get(instance)) {
-                trainingData[imageIndex] = flattenImage(makeTiny(image));
-                imageIndex++;
-            }
-
-        }
-
-        FloatNearestNeighboursExact knn = new FloatNearestNeighboursExact(trainingData);
-
-
-        for (String instance : testing.getGroups()) {
-            for (FImage image : testing.get(instance)) {
-                knn.searchKNN(flattenImage(makeTiny(image)), 10);
-            }
-        }
-
-
-
-
-
-        ClassificationEvaluator<CMResult<String>, String, FImage> eval =
-                new ClassificationEvaluator<CMResult<String>, String, FImage>(
-                        knn, testing, new CMAnalyser<FImage, String>(CMAnalyser.Strategy.SINGLE));
-
-        Map<Record<FImage>, ClassificationResult<String>> guesses = eval.evaluate();
-        CMResult<String> result = eval.analyse(guesses);
-
-
-    }
+    private FloatNearestNeighboursExact knn;
+    private String[] trainingAnnotations;
 
     public FImage makeTiny(FImage image) {
         int size = Math.min(image.width, image.height);
@@ -81,8 +45,12 @@ public class Classifier1 extends BatchAnnotator<FImage, String> {
     }
 
     @Override
-    public List<ScoredAnnotation<String>> annotate(FImage object) {
-        return null;
+    public List<ScoredAnnotation<String>> annotate(FImage image) {
+        IntFloatPair nearest = this.knn.searchNN(flattenImage(makeTiny(image)));
+        List<ScoredAnnotation<String>> result = new ArrayList<ScoredAnnotation<String>>(1);
+        result.add(new ScoredAnnotation<String>(this.trainingAnnotations[nearest.getFirst()], 1f));
+        System.out.println(result);
+        return result;
     }
 
     @Override
@@ -90,17 +58,20 @@ public class Classifier1 extends BatchAnnotator<FImage, String> {
 
         this.annotations = training.getGroups();
         float[][] trainingData = new float[training.numInstances()][16*16];
+        this.trainingAnnotations = new String[trainingData.length];
 
         int imageIndex = 0;
         for (String instance : training.getGroups()) {
+            System.out.println(instance);
             for (FImage image : training.get(instance)) {
                 trainingData[imageIndex] = flattenImage(makeTiny(image));
+                trainingAnnotations[imageIndex] = instance;
                 imageIndex++;
             }
 
         }
 
-        FloatNearestNeighboursExact knn = new FloatNearestNeighboursExact(trainingData);
+        this.knn = new FloatNearestNeighboursExact(trainingData);
 
 
     }
--- src/main/java/ImageRecognition.java
+++ src/main/java/ImageRecognition.java
@@ -1,22 +1,30 @@
+import org.openimaj.data.dataset.VFSGroupDataset;
 import org.openimaj.data.dataset.VFSListDataset;
+import org.openimaj.experiment.evaluation.classification.ClassificationEvaluator;
+import org.openimaj.experiment.evaluation.classification.ClassificationResult;
+import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMAnalyser;
+import org.openimaj.experiment.evaluation.classification.analysers.confusionmatrix.CMResult;
 import org.openimaj.image.DisplayUtilities;
 import org.openimaj.image.FImage;
 import org.openimaj.image.ImageUtilities;
 
 import java.nio.file.Paths;
+import java.util.Map;
 
 public class ImageRecognition {
     public static void main(String[] args) {
         try {
-            VFSListDataset<FImage> testing = new VFSListDataset<>("zip:" + Paths.get("").toAbsolutePath() + "\\images\\testing.zip", ImageUtilities.FIMAGE_READER);
-            VFSListDataset<FImage> training = new VFSListDataset<>("zip:" + Paths.get("").toAbsolutePath() + "\\images\\training.zip", ImageUtilities.FIMAGE_READER);
+            final VFSGroupDataset<FImage> training = new VFSGroupDataset<>(Paths.get("").toAbsolutePath() + "\\images\\training\\training", ImageUtilities.FIMAGE_READER);
+            final VFSGroupDataset<FImage> testing = new VFSGroupDataset<>(Paths.get("").toAbsolutePath() + "\\images\\testing", ImageUtilities.FIMAGE_READER);
 
-            DisplayUtilities.display("Testing", testing);
-            DisplayUtilities.display("Training", training);
+            Classifier1 classifier1 = new Classifier1();
+            classifier1.train(training);
 
-            Classifier1 classifier1 = new Classifier1(testing, training);
-            Classifier2 classifier2 = new Classifier2(testing, training);
-            Classifier3 classifier3 = new Classifier3(testing, training);
+            ClassificationEvaluator<CMResult<String>, String, FImage> evaluator = new ClassificationEvaluator<>(classifier1, testing, new CMAnalyser<>(CMAnalyser.Strategy.SINGLE));
+            Map<FImage, ClassificationResult<String>> evaluation = evaluator.evaluate();
+
+            //Classifier2 classifier2 = new Classifier2(testing, training);
+            //Classifier3 classifier3 = new Classifier3(testing, training);
         } catch (Exception e) {
             e.printStackTrace();
         }
